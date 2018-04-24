dbadb52a7b0591841f8b2e247096bea1ce05bc9a
OWEN PENMAN <1736953@student.fife.ac.uk>
2018-04-24 10:50:59 +0100

    Added animations

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Rigidbody2D myRigidBody;
+    private Animator anim;
+    [SerializeField]
+    private float MovementSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        Debug.Log(vertical);
+        HandleMovement(horizontal, vertical);
+
+        GetComponent<Animator>().SetFloat("vertMoves", vertical);
+
+    }
+    private void HandleMovement(float horizontal, float vertical)
+    {
+        myRigidBody.velocity = new Vector2(horizontal * MovementSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, vertical * MovementSpeed);
+
+        if (vertical == 0 && horizontal == 0)
+        {
+            GetComponent<Animator>().SetBool("Idle", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("Idle", false);
+        }
+    }
+}
