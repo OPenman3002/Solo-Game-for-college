31b81ffea4e195bae3dc4ba99e72f233671a5d7d
OPenman3002 <Ozone3002@aol.com>
2018-05-15 22:02:51 +0100

    added structure of player health

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+	public class PlayerStatus
+    {
+        public int Health = 10;
+    }
+    public PlayerStatus playerstatus = new PlayerStatus();
+
+    public void DamagePlayer( int Damage )
+    {
+        playerstatus.Health -= Damage;
+        if(playerstatus.Health <= 0)
+        {
+            
+            _GM.KillPlayer(this);
+        }
+    }
+}
---
+++ Assets/Scripts/_GM.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _GM : MonoBehaviour {
+       
+    public static void KillPlayer (PlayerStats playerstats) 
+    {
+        Destroy (playerstats.gameObject);
+    }
+	
+}
